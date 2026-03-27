b717032d8d0d220d6acb7c3b9ff9426c874c5509
James Maverick <jamesmaverickandhdcs@gmail.com>
2026-03-28 04:44:58 +0700

    chore: remove debug scripts from repo root

--- read_mad.py
+++
@@ -1,6 +0,0 @@
-FILE = r'C:\HDCS_Project\03\GNI_Autonomous\src\app\page.tsx'
-with open(FILE, 'r', encoding='utf-8', errors='surrogateescape') as f:
-    content = f.read()
-print('File length:', len(content))
-for term in ['latestArticles', 'Three Pillar', 'Quadratic']:
-    print(f'{term}:', content.find(term))
--- read_mad2.py
+++
@@ -1,7 +0,0 @@
-FILE = r'C:\HDCS_Project\03\GNI_Autonomous\src\app\page.tsx'
-with open(FILE, 'r', encoding='utf-8', errors='surrogateescape') as f:
-    content = f.read()
-
-idx = content.find('bg-yellow-900 border border-yellow-700')
-print('Disclaimer at:', idx)
-print(repr(content[idx-300:idx+50]))
