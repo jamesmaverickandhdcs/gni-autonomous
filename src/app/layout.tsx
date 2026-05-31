40ce7cb3190614d965d70be5f602f05cb311d0de
James Maverick <jamesmaverickandhdcs@gmail.com>
2026-05-31 07:33:30 +0700

    ui: remove PHI-003 banner from all pages (layout render + import) [S40]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Analytics } from '@vercel/analytics/react'
-import ResetBanner from '@/components/ResetBanner'
 
 const siteUrl = 'https://gni-autonomous.vercel.app'
 
@@ -78,7 +77,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn("font-sans", GeistSans.variable, GeistMono.variable)}>
       <body className="min-h-screen bg-background antialiased">
-        <ResetBanner />
         {children}
         <Analytics />
       </body>
